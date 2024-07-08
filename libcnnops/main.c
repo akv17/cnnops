d834e9606283f45fdbb592fe947df627b5b45d96
akv17 <jogarbello@gmail.com>
2024-07-08 17:12:42 +0300

    support arbitrary number of dims in matmul; refactor matmul

--- libcnnops/main.c
+++ libcnnops/main.c
@@ -6,22 +6,22 @@
 #include "timer.h"
 #include "ops/matmul.h"
 
-#define TENSOR_BUFFER_PRINT_LIMIT 16
+#define TENSOR_BUFFER_PRINT_LIMIT 8
 
 
 int main() {
     Tensor *a = read_tensor("a.bin", "a");
     Tensor *b = read_tensor("b.bin", "b");
     Tensor *c_in = read_tensor("c.bin", "c_in");
-    print_tensor(a, TENSOR_BUFFER_PRINT_LIMIT);
-    print_tensor(b, TENSOR_BUFFER_PRINT_LIMIT);
-    print_tensor(c_in, TENSOR_BUFFER_PRINT_LIMIT);
+    print_tensor(a, TENSOR_BUFFER_PRINT_LIMIT); printf("\n");
+    print_tensor(b, TENSOR_BUFFER_PRINT_LIMIT); printf("\n");
+    print_tensor(c_in, TENSOR_BUFFER_PRINT_LIMIT); printf("\n");
 
     timer_start();
-    Tensor *c_out = matmul_cpu_float32(a, b);
+    Tensor *c_out = matmul(a, b);
     timer_end();
     
-    // print_tensor(c_out, TENSOR_BUFFER_PRINT_LIMIT);
+    print_tensor(c_out, TENSOR_BUFFER_PRINT_LIMIT); printf("\n");
 
     timer_print();
 }
--- libcnnops/ops/matmul.h
+++ libcnnops/ops/matmul.h
@@ -4,76 +4,147 @@
 #include "../tensor.h"
 #include "../timer.h"
 
-#define _idx(i, j, s) (i * s + j)
+#define _idx2d(i, j, s) (i * s + j)
 
 
-Tensor *matmul_cpu_float32(Tensor *a, Tensor *b);
+Tensor *matmul(Tensor *a, Tensor *b);
+
+typedef struct OpSpec OpSpec;
+struct OpSpec {
+    int32_t ndim;
+    int32_t item_size;
+    int32_t *shape;
+
+    int32_t a_rows;
+    int32_t a_cols;
+    int32_t a_stride;
+    
+    int32_t b_rows;
+    int32_t b_cols;
+    int32_t b_stride;
+    
+    int32_t c_rows;
+    int32_t c_cols;
+    int32_t c_stride;
+    int32_t c_size;
+    
+    int32_t batch_dims;
+    int32_t batch_size;
+    int32_t batch_stride;
+};
+
+
+OpSpec *_compute_op_spec(Tensor *a, Tensor *b) {
+    int32_t ndim = a->num_dims;
+    int32_t a_rows = a->shape[ndim-2];
+    int32_t a_cols = a->shape[ndim-1];
+    int32_t a_stride = a_cols;
+    int32_t b_rows = b->shape[ndim-2];
+    int32_t b_cols = b->shape[ndim-1];
+    int32_t b_stride = b_cols;
+
+    int32_t batch_dims = ndim - 2;
+    int32_t batch_size = 1;
+    for (size_t i = 0; i < batch_dims; i++) {
+        batch_size *= a->shape[i];
+    }
+
+    int32_t c_rows = a_rows;
+    int32_t c_cols = b_cols;
+    int32_t c_stride = c_cols;
+    int32_t c_size = c_rows * c_cols * batch_size;
+    int32_t batch_stride = c_rows * c_cols;
+
+    int32_t *c_shape = (int32_t *) malloc(sizeof(int32_t) * ndim);
+    for (size_t i = 0; i < ndim; i++) {
+        c_shape[i] = a->shape[i];
+    }
+    c_shape[ndim-2] = c_rows;
+    c_shape[ndim-1] = c_cols;
+
+    OpSpec *spec = (OpSpec *) malloc(sizeof(OpSpec));
+    spec->ndim = ndim;
+    spec->item_size = a->item_size;
+    spec->shape = c_shape;
+    
+    spec->a_rows = a_rows;
+    spec->a_cols = a_cols;
+    spec->a_stride = a_stride;
+    
+    spec->b_rows = b_rows;
+    spec->b_cols = b_cols;
+    spec->b_stride = b_stride;
+
+    spec->c_rows = c_rows;
+    spec->c_cols = c_cols;
+    spec->c_stride = c_stride;
+    spec->c_size = c_size;
+
+    spec->batch_dims = batch_dims;
+    spec->batch_size = batch_size;
+    spec->batch_stride = batch_stride;
+    return spec;
+}
+
+
+Tensor *_create_output_tensor(OpSpec *spec, float32_t *buffer) {
+    Tensor *t = (Tensor *) malloc(sizeof(Tensor));
+    t->name = "c";
+    t->dtype = 0;
+    t->item_size = spec->item_size;
+    t->num_items = spec->c_size;
+    t->num_dims = spec->ndim;
+    t->shape = spec->shape;
+    t->buffer = buffer;
+    return t;
+}
 
 
 void _kernel(
+    int32_t offset,
     float32_t *a,
     float32_t *b,
     float32_t *c,
     int32_t a_rows,
-    int32_t a_col_str,
+    int32_t a_stride,
     int32_t b_cols,
-    int32_t b_col_str,
+    int32_t b_stride,
     int32_t common_dim,
-    int32_t c_col_str
+    int32_t c_stride
 ) {
     #pragma omp parallel for
     for (int i = 0; i < a_rows; i++) {
         for (int j = 0; j < b_cols; j++) {
             float32_t c_acc = 0.0;
             for (int k = 0; k < common_dim; k++) {
-                c_acc += a[_idx(i, k, a_col_str)] * b[_idx(k, j, b_col_str)];
+                c_acc += a[_idx2d(i, k, a_stride) + offset] * b[_idx2d(k, j, b_stride) + offset];
             }
-            c[_idx(i, j, c_col_str)] = c_acc;
+            c[_idx2d(i, j, c_stride) + offset] = c_acc;
         }
     }
 }
 
 
-Tensor *matmul_cpu_float32(Tensor *a, Tensor *b) {
+Tensor *matmul(Tensor *a, Tensor *b) {
+    OpSpec *spec = _compute_op_spec(a, b);
     float32_t *a_buf = (float32_t *) a->buffer;
     float32_t *b_buf = (float32_t *) b->buffer;
-
-    int32_t a_rows = a->shape[0];
-    int32_t a_cols = a->shape[1];
-    int32_t b_rows = b->shape[0];
-    int32_t b_cols = b->shape[1];
-    int32_t common_dim = a_cols;
-    int32_t a_col_str = a_cols;
-    int32_t b_col_str = b_cols;
-    
-    int32_t c_rows = a_rows;
-    int32_t c_cols = b_cols;
-    int32_t c_col_str = c_cols;
-    int32_t c_size = c_rows * c_cols;
-    float32_t *c_buf = (float32_t *) malloc(sizeof(float32_t) * c_size);
-
-    _kernel(
-        a_buf,
-        b_buf,
-        c_buf,
-        a_rows,
-        a_col_str,
-        b_cols,
-        b_col_str,
-        common_dim,
-        c_col_str
-    );
-
-    Tensor *t = (Tensor *) malloc(sizeof(Tensor));
-    int32_t *t_shape = (int32_t *) malloc(sizeof(int32_t) * 2);
-    t_shape[0] = c_rows;
-    t_shape[1] = c_cols;
-    t->name = "c";
-    t->dtype = 0;
-    t->item_size = a->item_size;
-    t->num_items = c_size;
-    t->num_dims = 2;
-    t->shape = t_shape;
-    t->buffer = c_buf;
-    return t;
+    float32_t *c_buf = (float32_t *) malloc(sizeof(float32_t) * spec->c_size);
+    int32_t batch_stride = spec->batch_stride;
+    for (int32_t offset = 0; offset < spec->batch_stride; offset += batch_stride) {
+        _kernel(
+            offset,
+            a_buf,
+            b_buf,
+            c_buf,
+            spec->a_rows,
+            spec->a_stride,
+            spec->b_cols,
+            spec->b_stride,
+            spec->a_cols,
+            spec->c_stride
+        );
+    }
+    Tensor *out = _create_output_tensor(spec, c_buf);
+    return out;
 }
